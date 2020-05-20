74a5110a6f4ac65728d30728a284ca8ccc17475c
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-05-20 20:42:27 +0200

    Implemented basic IEditable

--- TestIEditable/MainWindowViewModel.cs
+++ TestIEditable/MainWindowViewModel.cs
@@ -1,22 +1,62 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 
 namespace TestIEditable
 {
-    public class MainWindowViewModel: ViewModelBase
+    public class MainWindowViewModel : ViewModelBase
     {
-        private ObservableCollection<PersonViewModel> _persons=new ObservableCollection<PersonViewModel>();
-        public ObservableCollection<PersonViewModel> Persons { get => _persons; set { Set(() => Persons, ref _persons, value); }}
+        private ObservableCollection<PersonViewModel> _persons = new ObservableCollection<PersonViewModel>();
+        public ObservableCollection<PersonViewModel> Persons { get => _persons; set => Set(() => Persons, ref _persons, value); }
+        
+        private PersonViewModel  _selectedPerson; 
+        public PersonViewModel  SelectedPerson { get => _selectedPerson; set { value.BeginEdit(); Set(() => SelectedPerson, ref _selectedPerson, value); }}
+        
+        /*
+        private RelayCommand _addPersonCmd;
+        public RelayCommand AddPersonCmd => _addPersonCmd ?? (_addPersonCmd = new RelayCommand(
+            () => addPersonCmd(),
+            () => { return 1 == 1; },
+            keepTargetAlive: true
+            ));
+
+        private void addPersonCmd()
+        {
+            throw new NotImplementedException();
+        }
+        */
+
+        private RelayCommand<PersonViewModel> _savePersonCmd;
+        public RelayCommand<PersonViewModel> SavePersonCmd => _savePersonCmd ?? (_savePersonCmd = new RelayCommand<PersonViewModel>(
+            (pvm) => savePersonCmd(pvm),
+            (pvm) => pvm != null,
+            keepTargetAlive: true
+            ));
+
+        private void savePersonCmd(PersonViewModel personViewModel)
+        {
+            personViewModel.EndEdit();
+        }
+
+
+        private RelayCommand<PersonViewModel> _cancelPersonCmd;
+        public RelayCommand<PersonViewModel> CancelPersonCmd => _cancelPersonCmd ?? (_cancelPersonCmd = new RelayCommand<PersonViewModel>(
+            (pvm) => cancelPersonCmd(pvm),
+            (pvm) => pvm != null,
+            keepTargetAlive: true
+            ));
+
+        private void cancelPersonCmd(PersonViewModel personViewModel)
+        {
+            personViewModel.CancelEdit();
+        }
 
         public MainWindowViewModel()
         {
-            Persons.Add(new PersonViewModel( "uno",1));
-            Persons.Add(new PersonViewModel("due",2));
-            Persons.Add(new PersonViewModel("tre",3));
+            Persons.Add(new PersonViewModel("uno", 1));
+            Persons.Add(new PersonViewModel("due", 2));
+            Persons.Add(new PersonViewModel("tre", 3));
         }
     }
 }
--- TestIEditable/PersonViewModel.cs
+++ TestIEditable/PersonViewModel.cs
@@ -1,22 +1,43 @@
 ﻿using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 
 namespace TestIEditable
 {
-    public class PersonViewModel: ViewModelBase
+    public class PersonViewModel: ViewModelBase, IEditableObject
     {
-
         private string _name; public string Name { get => _name; set { Set(() => Name, ref _name, value); }}
-
         private int _age; public int Age { get => _age; set { Set(() => Age, ref _age, value); }}
+
+        private PersonViewModel tempPersonViewModel;
 
         public PersonViewModel(string name, int age)
         {
             Name = name;
             Age = age;
         }
+
+        public void BeginEdit()
+        {
+            tempPersonViewModel = new PersonViewModel(Name, Age);
+
+        }
+
+        public void CancelEdit()
+        {
+            if (tempPersonViewModel == null)
+                return;
+            Name = tempPersonViewModel.Name;
+            Age = tempPersonViewModel.Age;
+        }
+         
+        public void EndEdit()
+        {
+            tempPersonViewModel = null;
+        }
+
     }
 }
