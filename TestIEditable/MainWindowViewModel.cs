239ee1f1d501f981569148285521e39e93fa6755
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-05-20 20:16:35 +0200

    Add project files.

---
+++ TestIEditable/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace TestIEditable
+{
+    public class MainWindowViewModel: ViewModelBase
+    {
+        private ObservableCollection<PersonViewModel> _persons=new ObservableCollection<PersonViewModel>();
+        public ObservableCollection<PersonViewModel> Persons { get => _persons; set { Set(() => Persons, ref _persons, value); }}
+
+        public MainWindowViewModel()
+        {
+            Persons.Add(new PersonViewModel( "uno",1));
+            Persons.Add(new PersonViewModel("due",2));
+            Persons.Add(new PersonViewModel("tre",3));
+        }
+    }
+}
---
+++ TestIEditable/PersonViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestIEditable
+{
+    public class PersonViewModel: ViewModelBase
+    {
+
+        private string _name; public string Name { get => _name; set { Set(() => Name, ref _name, value); }}
+
+        private int _age; public int Age { get => _age; set { Set(() => Age, ref _age, value); }}
+
+        public PersonViewModel(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
